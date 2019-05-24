d6d88f6bf198ac8cfee7f3b39f4bc5801e618315
ciechann <emgasuk@gmail.com>
2019-05-24 23:14:20 +0100

    final

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -3,5 +3,18 @@ package com.sda.algorytmy;
 public class App {
     public static void main(String[] args) {
 
+        int[] tab = new int[]{2, 1, 3, 4, 9, 6, 7, 8};
+
+        for (int i = 0; i < tab.length - 1; i++) {
+            int a = 0;
+            if (tab[i] > tab[i + 1]) {
+                a = tab[i];
+                tab[i] = tab[i + 1];
+                tab[i + 1] = a;
+            }
+        }
+        for (int i = 0; i < tab.length; i++) {
+            System.out.println(tab[i]);
+        }
     }
 }
